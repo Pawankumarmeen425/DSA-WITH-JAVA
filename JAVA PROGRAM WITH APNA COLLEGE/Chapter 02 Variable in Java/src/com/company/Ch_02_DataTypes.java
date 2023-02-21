417f558e3c8dd7ac9fbdfd5e815bfb1de52b9326
Pawankumarmeen425 <112488015+Pawankumarmeen425@users.noreply.github.com>
2023-02-21 15:26:18 +0530

    Delete JAVA PROGRAM WITH APNA COLLEGE directory

--- JAVA PROGRAM WITH APNA COLLEGE/Chapter 02 Variable in Java/src/com/company/Ch_02_DataTypes.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Ch_02_DataTypes {
-    public static void main(String[] args) {
-        for (int i=0;;i++){
-            System.out.println("Hello World!!");
-        }
-    }
-}
